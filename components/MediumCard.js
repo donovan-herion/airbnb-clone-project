dd607eb4e097fc3e0dd051a221b02596107c1451
donovan-herion <donovanherion@gmail.com>
2022-02-03 18:58:53 +0100

    redid buiild

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 function MediumCard({img, title}) {
     return (
-        <div className="cursor-pointer hover:scale-[103%] transition-all duration-300 ease-out scrollbar-hide p-3 -ml-3">
-            <div className="relative h-80 w-80">
+        <div className="cursor-pointer  scrollbar-hide p-3 -ml-3">
+            <div className="relative h-80 w-80 hover:scale-[103%] transition-all duration-300 ease-out">
                 <Image src={img} layout="fill" className="rounded-lg"></Image>
             </div>
             <h3 className="text-2xl mt-3">{title}</h3>
